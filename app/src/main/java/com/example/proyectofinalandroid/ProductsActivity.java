416c7df9232f13fc471b273bf96ac19808f333c4
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-07-18 20:08:23 -0500

    :tada: Initial commit.

---
+++ app/src/main/java/com/example/proyectofinalandroid/ProductsActivity.java
@@ -0,0 +1,38 @@
+package com.example.proyectofinalandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProductsActivity extends AppCompatActivity {
+
+    Button btnSignOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+
+        btnSignOut = (Button) findViewById(R.id.btnSignOut);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        // [START auth_sign_out]
+        FirebaseAuth.getInstance().signOut();
+        // [END auth_sign_out]
+        com.facebook.login.LoginManager.getInstance().logOut();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
