15831e6d67a61f63c998a0eb10696f739660a1e4
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-07-19 06:18:53 -0500

    :sparkles: Estructura y estilos del formulario de registro de productos agregado.

--- app/src/main/java/com/example/proyectofinalandroid/InsertProductActivity.java
+++ app/src/main/java/com/example/proyectofinalandroid/InsertProductActivity.java
@@ -3,12 +3,25 @@ package com.example.proyectofinalandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class InsertProductActivity extends AppCompatActivity {
+
+    private Button btnOpenCamara, btnInsertProduct, btnExit;
+    private EditText edtProductName, edtProductPrice;
+    private ImageView ivProductImg;
+    private String userUID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_product);
+
+        userUID = getIntent().getStringExtra("userUid");
+        edtProductName = (EditText) findViewById(R.id.edt_product_name);
+        edtProductName = (EditText) findViewById(R.id.edt_product_price);
+        ivProductImg = (ImageView) findViewById(R.id.iv_product_image);
     }
 }
